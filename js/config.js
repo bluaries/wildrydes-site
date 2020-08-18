41f9630a8cba5178e5ed28d933707852f2c65ea1
kidstylex <chilizhiki@gmail.com>
2020-08-19 00:14:50 +0700

    modified config.js

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
         userPoolId: 'us-east-1_smltMRuk7', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '1maoeuf5cnrpi69qavp76gs5i9', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: 'cs13grjuj7m8fh2km6h11mnjv', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
-};
+}; 
