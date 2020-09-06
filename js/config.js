f38360fbb17910cdd71ad775710848008d434a26
kidstylex <chilizhiki@gmail.com>
2020-09-06 15:29:03 +0700

    Change config

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_QYehA6Icb', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: 'cs13grjuj7m8fh2km6h11mnjv', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_kYomR5ugL', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '7rni6m057q50mc3snkn1hddu3', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'https://6g6gvym2l0.execute-api.us-east-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 }; 
